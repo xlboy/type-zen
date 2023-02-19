080a9ab8e5b4e8b245067cf991e09459eb2b303e
xlboy <xlboy88888@foxmail.com>
2023-02-20 00:31:51 +0800

    test: 补充 condition 模板

--- src/__test__/__resource__/syntax/expressions/condition.ts
+++ src/__test__/__resource__/syntax/expressions/condition.ts
@@ -5,7 +5,7 @@ const conditionTemplate = {
     native: [
       "true extends false ? true : false",
       "123 extends 121 ? (1) : 2 extends 2 ? (3) : 4",
-      "998 extends 1 ? boolean extends c<ddd> ? (1) : 2 extends 2 ? (3) : 4 : 1 | 2 | didi<dd>",
+      "998 extends 1 ? boolean extends c<ddd> ? (1) : 2 extends 2 ? (3) : 4 : (1 | 2 | didi<dd>)",
     ],
     extended: [
       "123 == 121 ? 1 : 2 == 2 ? 3 : 4",
