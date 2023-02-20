8052ff075d8810bffd4a2f9c9f8ff99cc844f5c9
xlboy <xlboy88888@foxmail.com>
2023-02-20 15:04:08 +0800

    test: 重构 `literal` 用例

--- src/__test__/__resource__/syntax/expressions/literal.ts
+++ src/__test__/__resource__/syntax/expressions/literal.ts
@@ -1,36 +1,112 @@
-export { literalTemplate };
+import * as utils from "../../utils";
+import * as ast from "../../../../ast";
 
-const literalTemplate = {
-  number: {
-    valid: [
-      "0",
-      "8",
-      "1921291",
-      "1.11",
-      "11.1",
-      "11.1",
-      "0.111",
-      "-1.11",
-      "-11.1",
-      "-11.1",
-      "2342341.111111111",
-    ],
-    invalid: ["1.1.1", "1231.111-", "1231.111+", "1231.111e", "1231.111e-"],
-  },
-  string: {
-    valid: [
-      '"123"',
-      "'1111'",
-      "`wwlklksdldfs.1111`",
-      '"钅耂阝赛三三三朩"',
-      "` 嗯？三地方似懂非懂赛  非非`",
-    ],
-    invalid: [
-      '"123',
-      "'1111",
-      "`wwlklksdldfs.1111",
-      "钅耂阝赛三三三朩",
-      "123,..？？？。*！！@）&￥@&￥啊哦好",
-    ],
-  },
-};
+export { Literal };
+
+namespace Literal {
+  export const template = {
+    number: {
+      valid: [
+        "0",
+        "8",
+        "1921291",
+        "1.11",
+        "11.1",
+        "11.1",
+        "0.111",
+        "-1.11",
+        "-11.1",
+        "-11.1",
+        "2342341.111111111",
+      ],
+      invalid: ["1.1.1", "1231.111-", "1231.111+", "1231.111e", "1231.111e-"],
+    },
+    string: {
+      valid: [
+        '"123"',
+        "'1111'",
+        "`wwlklksdldfs.1111`",
+        '"钅耂阝赛三三三朩"',
+        "` 嗯？三地方似懂非懂赛  非非`",
+      ],
+      invalid: [
+        '"123',
+        "'1111",
+        "`wwlklksdldfs.1111",
+        "钅耂阝赛三三三朩",
+        "123,..？？？。*！！@）&￥@&￥啊哦好",
+      ],
+    },
+  } as const;
+
+  export const nodes = {
+    number: template.number.valid.map((numLiteral) => {
+      const contents = ["type a =     ", numLiteral];
+      const content = utils.mergeString(...contents);
+
+      return utils.createSource({
+        content,
+        nodes: [
+          utils.createNode<ast.TypeDeclarationStatement>({
+            instance: ast.TypeDeclarationStatement,
+            kind: ast.Type.SyntaxKind.S.TypeDeclaration,
+            pos: {
+              start: { line: 1, col: 1 },
+              end: { line: 1, col: content.length + 1 },
+            },
+            output: utils.mergeString("type a = ", numLiteral, ";"),
+            identifier: {
+              instance: ast.IdentifierExpression,
+              kind: ast.Type.SyntaxKind.E.Identifier,
+              output: "a",
+              pos: { start: { line: 1, col: 6 }, end: { line: 1, col: 7 } },
+            },
+            value: {
+              instance: ast.NumberLiteralExpression,
+              kind: ast.Type.SyntaxKind.E.NumberLiteral,
+              output: numLiteral,
+              pos: {
+                start: { line: 1, col: contents[0].length + 1 },
+                end: { line: 1, col: content.length + 1 },
+              },
+            },
+          }),
+        ],
+      });
+    }),
+    string: template.string.valid.map((strLiteral) => {
+      const contents = ["type strrr =", strLiteral];
+      const content = utils.mergeString(...contents);
+
+      return utils.createSource({
+        content,
+        nodes: [
+          utils.createNode<ast.TypeDeclarationStatement>({
+            instance: ast.TypeDeclarationStatement,
+            kind: ast.Type.SyntaxKind.S.TypeDeclaration,
+            pos: {
+              start: { line: 1, col: 1 },
+              end: { line: 1, col: content.length + 1 },
+            },
+            output: utils.mergeString("type strrr = ", strLiteral, ";"),
+            identifier: {
+              instance: ast.IdentifierExpression,
+              kind: ast.Type.SyntaxKind.E.Identifier,
+              output: "strrr",
+              pos: { start: { line: 1, col: 6 }, end: { line: 1, col:  11 } },
+            },
+            value: {
+              instance: ast.StringLiteralExpression,
+              kind: ast.Type.SyntaxKind.E.StringLiteral,
+              output: strLiteral,
+              pos: {
+                start: { line: 1, col: contents[0].length + 1 },
+                end: { line: 1, col: content.length + 1 },
+              },
+            },
+          }),
+        ],
+      });
+    }),
+  };
+}
--- src/__test__/parse/literal.test.ts
+++ src/__test__/parse/literal.test.ts
@@ -1,50 +1,12 @@
 import { describe, expect, it } from "vitest";
 import { NearleyError, Parser } from "../../api/parser";
+import * as resource from "../__resource__";
 import * as ast from "../../ast";
-import { literalTemplate } from "../__resource__/syntax/expressions/literal";
 
 describe("number", () => {
-  describe("valid", () => {
-    function forFn(fn: (numberLiteral: string) => void) {
-      for (const numberLiteral of literalTemplate.number.valid) {
-        fn(numberLiteral);
-      }
-    }
-
-    it("should not throw", () => {
-      forFn((numberLiteral) => {
-        const fn = () => new Parser(`type a =   ${numberLiteral};`).toAST();
-        expect(fn).not.throw();
-      });
-    });
-
-    it("ast node", () => {
-      forFn((numberLiteral) => {
-        const contents = ["type b1 =       ", numberLiteral, ";;;;;;;;;;"];
-        const contentStr = contents.join("");
-        const astNodes = new Parser(contentStr).toAST();
-
-        expect(astNodes.length).not.toBe(0);
-        expect(astNodes[0]).instanceOf(ast.Base);
-
-        const node = astNodes[0] as ast.TypeDeclarationStatement;
-        expect(node.compile()).toBe(`type b1 = ${numberLiteral};`);
-        expect(node.kind).toBe(ast.Type.SyntaxKind.S.TypeDeclaration);
-        expect(node.pos).toMatchObject({
-          start: { line: 1, col: 1 },
-          end: { line: 1, col: `${contents[0]}${contents[1]}`.length + 1 },
-        } as ast.Type.Position);
-
-        expect(node.value).instanceOf(ast.NumberLiteralExpression);
-        expect(node.value.kind).toBe(ast.Type.SyntaxKind.E.NumberLiteral);
-        expect(node.value.compile()).toBe(numberLiteral);
-      });
-    });
-  });
-
-  it("invalid", () => {
-    for (const numberLiteral of literalTemplate.number.invalid) {
-      const fn = () => new Parser(`type b = ${numberLiteral};`).toAST();
+  it("error throw: UnexpectedInput", () => {
+    for (const num of resource.Literal.template.number.invalid) {
+      const fn = () => new Parser(`type b = ${num};`).toAST();
       expect(fn).throw();
 
       try {
@@ -53,59 +15,32 @@ describe("number", () => {
         expect(error).instanceOf(NearleyError.UnexpectedInput);
       }
     }
+  });
+
+  it("nodes", () => {
+    resource.Literal.nodes.number.forEach((node) => {
+      resource.utils.assertSource(node);
+    });
   });
 });
 
 describe("string", () => {
-  describe("valid", () => {
-    function forFn(fn: (stringLiteral: string) => void) {
-      for (const stringLiteral of literalTemplate.string.valid) {
-        fn(stringLiteral);
+    it("error throw: UnexpectedInput", () => {
+      for (const str of resource.Literal.template.string.invalid) {
+        const fn = () => new Parser(`type str = ${str};`).toAST();
+        expect(fn).throw();
+  
+        try {
+          fn();
+        } catch (error) {
+          expect(error).instanceOf(NearleyError.UnexpectedInput);
+        }
       }
-    }
-
-    it("should not throw", () => {
-      forFn((stringLiteral) => {
-        const fn = () =>
-          new Parser(`type str___ =   ${stringLiteral};`).toAST();
-        expect(fn).not.throw();
-      });
     });
-
-    it("ast node", () => {
-      forFn((stringLiteral) => {
-        const contents = ["type ssss =", stringLiteral];
-        const contentStr = contents.join("");
-        const astNodes = new Parser(contentStr).toAST();
-
-        expect(astNodes.length).not.toBe(0);
-        expect(astNodes[0]).instanceOf(ast.TypeDeclarationStatement);
-
-        const node = astNodes[0] as ast.TypeDeclarationStatement;
-        expect(node.compile()).toBe(`type ssss = ${stringLiteral};`);
-        expect(node.kind).toBe(ast.Type.SyntaxKind.S.TypeDeclaration);
-        expect(node.pos).toMatchObject({
-          start: { line: 1, col: 1 },
-          end: { line: 1, col: `${contents[0]}${contents[1]}`.length + 1 },
-        } as ast.Type.Position);
-
-        expect(node.value).instanceOf(ast.StringLiteralExpression);
-        expect(node.value.kind).toBe(ast.Type.SyntaxKind.E.StringLiteral);
-        expect(node.value.compile()).toBe(stringLiteral);
+  
+    it("nodes", () => {
+      resource.Literal.nodes.string.forEach((node) => {
+        resource.utils.assertSource(node);
       });
     });
   });
-
-  it("invalid", () => {
-    for (const stringLiteral of literalTemplate.string.invalid) {
-      const fn = () => new Parser(`type str = ${stringLiteral};`).toAST();
-      expect(fn).throw();
-
-      try {
-        fn();
-      } catch (error) {
-        expect(error).instanceOf(NearleyError.UnexpectedInput);
-      }
-    }
-  });
-});
