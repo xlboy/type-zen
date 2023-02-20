7badc086573d2dc2bcc1e16519d6c56c844d498b
xlboy <xlboy88888@foxmail.com>
2023-02-20 21:47:23 +0800

    test: 重构 `condition` 用例

--- src/__test__/__resource__/index.ts
+++ src/__test__/__resource__/index.ts
@@ -2,6 +2,7 @@ export { Literal } from "./syntax/expressions/literal";
 export { Union } from "./syntax/expressions/union";
 export { BracketSurround } from "./syntax/expressions/bracket-surround";
 export { Identifier } from "./syntax/expressions/identifier";
+export { Condition } from "./syntax/expressions/condition";
 
 import { assertSource } from "./utils";
 
--- src/__test__/__resource__/syntax/expressions/condition.ts
+++ src/__test__/__resource__/syntax/expressions/condition.ts
@@ -1,16 +1,117 @@
-export { conditionTemplate };
+import * as utils from "../../utils";
+import * as ast from "../../../../ast";
 
-const conditionTemplate = {
-  valid: {
+export { Condition };
+
+namespace Condition {
+  const template = {
+    valid: {
+      native: [
+        "true extends false ? true : false",
+        "123 extends 121 ? (1) : 2 extends 2 ? (3) : 4",
+        "998 extends 1 ? boolean extends c<ddd> ? (1) : 2 extends 2 ? (3) : 4 : (1 | 2 | didi<dd>)",
+      ],
+      extended: [
+        "123 == 121 ? 1 : 2 == 2 ? 3 : 4",
+        "(123 == 121 ? 1 : 2) == 2 ? 3 : 4",
+        '123 == 121 ? 1 : 2 == 2 ? 3 : (4 | 1 | true | "sss")',
+      ],
+    },
+  };
+
+  export const nodes = {
     native: [
-      "true extends false ? true : false",
-      "123 extends 121 ? (1) : 2 extends 2 ? (3) : 4",
-      "998 extends 1 ? boolean extends c<ddd> ? (1) : 2 extends 2 ? (3) : 4 : (1 | 2 | didi<dd>)",
+      utils.createSource({
+        content: `
+        type name = true extends false ? "true--" : 123; 
+        `,
+        nodes: [
+          utils.createNode({
+            instance: ast.TypeDeclarationStatement,
+            output: `type name = true extends false ? "true--" : 123;`,
+            identifier: utils.createNode({
+              instance: ast.IdentifierExpression,
+              output: "name",
+            }),
+            value: utils.createNode({
+              instance: ast.ConditionExpression,
+              output: `true extends false ? "true--" : 123`,
+              left: utils.createNode({
+                instance: ast.ValueKeywordExpression,
+                output: "true",
+              }),
+
+              right: utils.createNode({
+                instance: ast.ValueKeywordExpression,
+                output: "false",
+              }),
+              then: utils.createNode({
+                instance: ast.StringLiteralExpression,
+                output: `"true--"`,
+              }),
+              else: utils.createNode({
+                instance: ast.NumberLiteralExpression,
+                output: "123",
+              }),
+            }),
+          }),
+        ],
+      }),
     ],
+
     extended: [
-      "123 == 121 ? 1 : 2 == 2 ? 3 : 4",
-      "(123 == 121 ? 1 : 2) == 2 ? 3 : 4",
-      '123 == 121 ? 1 : 2 == 2 ? 3 : (4 | 1 | true | "sss")',
+      utils.createSource({
+        content: `
+        type name = 123 == 121 ? 1 : 2 == 2 ? 3 : 4;
+        `,
+        nodes: [
+          utils.createNode({
+            instance: ast.TypeDeclarationStatement,
+            output: `type name = 123 extends 121 ? 1 : 2 extends 2 ? 3 : 4;`,
+            identifier: utils.createNode({
+              instance: ast.IdentifierExpression,
+              output: "name",
+            }),
+            value: utils.createNode({
+              instance: ast.ConditionExpression,
+              output: `123 extends 121 ? 1 : 2 extends 2 ? 3 : 4`,
+              left: utils.createNode({
+                instance: ast.NumberLiteralExpression,
+                output: "123",
+              }),
+              right: utils.createNode({
+                instance: ast.NumberLiteralExpression,
+                output: "121",
+              }),
+              then: utils.createNode({
+                instance: ast.NumberLiteralExpression,
+                output: "1",
+              }),
+
+              else: utils.createNode({
+                instance: ast.ConditionExpression,
+                output: `2 extends 2 ? 3 : 4`,
+                left: utils.createNode({
+                  instance: ast.NumberLiteralExpression,
+                  output: "2",
+                }),
+                right: utils.createNode({
+                  instance: ast.NumberLiteralExpression,
+                  output: "2",
+                }),
+                then: utils.createNode({
+                  instance: ast.NumberLiteralExpression,
+                  output: "3",
+                }),
+                else: utils.createNode({
+                  instance: ast.NumberLiteralExpression,
+                  output: "4",
+                }),
+              }),
+            }),
+          }),
+        ],
+      }),
     ],
-  },
-};
+  };
+}
