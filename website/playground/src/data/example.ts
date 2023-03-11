b846917669f2d539123501fb167bb1abc70cbda5
xlboy <xlboy88888@foxmail.com>
2023-03-12 07:13:11 +0800

    feat(playground): 完善多特性（基本例子、例子在线分享等）

--- website/playground/src/data/example.ts
+++ website/playground/src/data/example.ts
@@ -45,6 +45,48 @@ type Example1 = ^{
   return [A, '-------', B]
 }
 `
+      },
+      {
+        key: 'sugar-if-else',
+        name: 'If Else',
+        zenCode: `
+type Example1<Name: string = ""> = ^{
+  type NameInfoMap = {
+    Kai: "Kai, Confident and intelligent.",
+    Mia: "Mia, Sophisticated and elegant.",
+    Jax: "Jax, Adventurous and fearless.",
+  };
+
+  if (Name == '') {
+    return 'Why not give me a name?'
+  }
+  
+  if (Name == keyof NameInfoMap) {
+    return NameInfoMap[Name]
+  }
+  
+  return [Name, " is a good name"]
+};
+
+type Test1 = Example1<"Kai">
+type Test2 = Example1<"Mia">
+type Test3 = Example1<"Jax">
+type Test4 = Example1<"Xlboy">
+type Test5 = Example1
+`
+      },
+      {
+        key: 'sugar-for',
+        name: 'For',
+        zenCode: `
+type GetTypeName<U> = ^{
+  for (infer Item in U) {
+   return [Item, ":("]
+  }
+};
+
+type Test1 = GetTypeName<| ["name", 123123, true, false, {}, () => void]>
+`
       }
     ]
   }
