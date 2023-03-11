bb312b72d4571dff266fb567e99bd1497221820c
xlboy <xlboy88888@foxmail.com>
2023-03-11 23:48:26 +0800

    chore(core): 支持 node/browser 包

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,11 +1,28 @@
-import { defineConfig } from 'tsup';
+import { defineConfig, type Options } from 'tsup';
 
-export default defineConfig({
+const commonConfig: Options = {
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
   clean: true,
-  platform: 'browser',
   dts: {
     resolve: true
+  },
+  outExtension(ctx) {
+    const fileSuffix = ctx.format === 'cjs' ? 'js' : 'mjs';
+
+    return {
+      js: `.${ctx.options.platform}.${fileSuffix}`
+    };
   }
-});
+};
+
+export default defineConfig([
+  {
+    ...commonConfig,
+    platform: 'node'
+  },
+  {
+    ...commonConfig,
+    platform: 'browser'
+  }
+]);
