4783b9bcb32fd7b446e99a47dc04f43927ef6aa8
xlboy <xlboy88888@foxmail.com>
2023-03-21 23:39:04 +0800

    feat(playground): add `tc-medium-(14-25)` example
    14. Replace
    15. ReplaceAll
    16. Append Argument
    17. Permutation
    18. Length of String
    19. Flatten
    20. Append to object
    21. Absolute
    22. String to Union
    23. Merge
    24. KebabCase
    25. Diff

--- website/playground/src/data/example/type-challenges/medium.ts
+++ website/playground/src/data/example/type-challenges/medium.ts
@@ -17,6 +17,18 @@ export const tcMediumExample = {
    * 11. Trim Left
    * 12. Trim
    * 13. Capitalize
+   * 14. Replace
+   * 15. ReplaceAll
+   * 16. Append Argument
+   * 17. Permutation
+   * 18. Length of String
+   * 19. Flatten
+   * 20. Append to object
+   * 21. Absolute
+   * 22. String to Union
+   * 23. Merge
+   * 24. KebabCase
+   * 25. Diff
    */
   children: [
     {
@@ -167,6 +179,165 @@ type MyCapitalize<S: string> = ^{
     return S
   }
 }
+`
+    },
+    {
+      key: 'type-challenges-medium-14_replace',
+      name: '14 - Replace',
+      zenCode: `
+type Replace<S: string, From: string, To: string> = ^{
+  type C = From == '' ? never : From
+
+  if (S == \`\${infer L}\${C & string}\${infer R}\`) {
+    return \`\${L}\${To}\${R}\`
+  } else {
+    return S
+  }
+}
+`
+    },
+    {
+      key: 'type-challenges-medium-15_replace-all',
+      name: '15 - ReplaceAll',
+      zenCode: `
+type ReplaceAll<S: string, From: string, To: string> = ^{
+  if (From == '') {
+    return S
+  }
+  
+  if (S == \`\${infer L}\${From}\${infer R}\`) {
+    return \`\${L}\${To}\${ReplaceAll<R, From, To> & string}\`
+  }
+
+  return S
+}
+`
+    },
+    {
+      key: 'type-challenges-medium-16_append-argument',
+      name: '16 - Append Argument',
+      zenCode: `
+type AppendArgument<Fn, A> = ^{
+  if (Fn == (...args: infer Args) => infer R) {
+    return (...args: [...Args, A]) => R
+  }
+}
+`
+    },
+    {
+      key: 'type-challenges-medium-17_permutation',
+      name: '17 - Permutation',
+      zenCode: `
+type Permutation<T, I = T> = ^{
+  if ([T] == never[]) {
+    return []
+  }
+
+  for (infer R in I) {
+    return [R, ...Permutation<Exclude<T, R>>]
+  }
+}
+`
+    },
+    {
+      key: 'type-challenges-medium-18_length-of-string',
+      name: '18 - Length of String',
+      zenCode: `
+type LengthOfString<S: string, Arr: any[] = []> = ^{
+  if (S == \`\${infer F}\${infer Rest}\`) {
+    return LengthOfString<Rest, [...Arr, F]>
+  }
+
+  return Arr['length']
+}
+`
+    },
+    {
+      key: 'type-challenges-medium-19_flatten',
+      name: '19 - Flatten',
+      zenCode: `
+type Flatten<T: any[]> = ^{
+  if (T == [...infer K, infer R]) {
+    if (R == any[]) {
+      return [...Flatten<K>, ...Flatten<R>]
+    } else {
+      return [...Flatten<K>, R]
+    }
+  }
+
+  return []
+}
+`
+    },
+    {
+      key: 'type-challenges-medium-20_append-to-object',
+      name: '20 - Append to Object',
+      zenCode: `
+type AppendToObject<T, U: string, V> = {
+  [key in keyof T | U]: key == keyof T ? T[key] : V;
+}
+`
+    },
+    {
+      key: 'type-challenges-medium-21_absolute',
+      name: '21 - Absolute',
+      zenCode: `
+type Absolute<T: number | string | bigint> = ^{
+  if (\`\${T}\` == \`-\${infer V}\`) {
+    return V
+  }
+  return \`\${T}\`
+}
+`
+    },
+    {
+      key: 'type-challenges-medium-22_string-to-union',
+      name: '22 - String to Union',
+      zenCode: `
+type StringToUnion<T: string> = ^{
+  if (T == \`\${infer A}\${infer B}\`) {
+    return A | StringToUnion<B>
+  }
+}
+`
+    },
+    {
+      key: 'type-challenges-medium-23_merge',
+      name: '23 - Merge',
+      zenCode: `
+type Merge<F: object, S: object> = {
+  [key in keyof F | keyof S]: ^{
+    if (key extends keyof S) {
+      return S[key]
+    } else if (key extends keyof F) {
+      return F[key]
+    }
+  }
+}      
+`
+    },
+    {
+      key: 'type-challenges-medium-24_kebab-case',
+      name: '24 - KebabCase',
+      zenCode: `
+type KebabCase<S: string> = ^{
+  if (S == \`\${infer F}\${infer R}\`) {
+    if (R == Uncapitalize<R>) {
+      return \`\${Lowercase<F>}\${KebabCase<R>}\`
+    } else {
+      return \`\${Lowercase<F>}-\${KebabCase<R>}\`
+    }
+  }
+
+  return S
+}
+`
+    },
+    {
+      key: 'type-challenges-medium-25_diff',
+      name: '25 - Diff',
+      zenCode: `
+type Diff<O, O1> = Omit<O & O1, keyof (O | O1)>
 `
     }
   ]
