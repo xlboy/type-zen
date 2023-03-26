5974f28f0243ca1a215aa30e104657908b07f118
xlboy <xlboy88888@foxmail.com>
2023-03-27 00:00:19 +0800

    chore(vscode/scripts): add vsce-package file

---
+++ packages/vscode/scripts/vsce-package.ts
@@ -0,0 +1,32 @@
+import { execa } from 'execa';
+import fs from 'fs-extra';
+import path from 'path';
+
+async function main(toPublish: boolean) {
+  const pkgPath = path.resolve(__dirname, '../package.json');
+  const rawJSON = await fs.readFile(pkgPath, 'utf-8');
+  const pkg = JSON.parse(rawJSON);
+
+  pkg.name = 'TypeZen';
+
+  await fs.writeJSON(pkgPath, pkg, { spaces: 2 });
+  try {
+    if (toPublish) {
+      await execa(
+        'vsce',
+        ['publish', '--no-dependencies', '-p', process.env.VSCE_TOKEN!],
+        { stdio: 'inherit' }
+      );
+    } else {
+      await execa('vsce', ['package', '--no-dependencies'], { stdio: 'inherit' });
+    }
+  } finally {
+    await fs.writeFile(pkgPath, rawJSON, 'utf-8');
+  }
+}
+
+// 获取执行脚本时传入的参数
+const args = process.argv.slice(2);
+const toPublish = args.includes('--publish');
+
+main(toPublish);
